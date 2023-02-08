e7aa2db0c1da4debe2ac6796c660247ffb36acfa
Zhandos <zhandos@kz.kz>
2023-02-08 23:06:39 +0600

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+// alert ('Hello')
+// const result = confirm('Are you here?')
+
+// console.log(result)
+
+/* const answer = -prompt ('How old are you?','18')
+
+// alert (answer+5)
+*/
+
+// const answers = []
+
+// answers[0] = prompt('Как вас зовут?', '')
+// answers[1] = prompt('Где вы живете?', '')
+// answers[2] = prompt('Сколько раз вы меняли работу?', '')
+
+// // alert (answers[0])
+// // alert (answers[1])
+// // alert (answers[2])
+
+// document.write(answers)
+
+// const cat = 'toys';
+
+// console.log (`https://mytoys.ru/${cat}/10`)
+
+// let incr = 10,
+//     decr = 10;
+
+// console.log(--incr, ++decr)
+
+// console.log(incr, decr)
+
+
+let a = true,
+    b = 10,
+    c = 'string',
+    d = false
+
+
+console.log(a && d && null) //если оба значеня не true, то выводится последнее
+
+console.log(null || b || false)
